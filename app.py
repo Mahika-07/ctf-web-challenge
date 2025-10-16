3c020e8f674480314994cdbbbaf303395e97cb32
Mahika-07 <mahikapatney@gmail.com>
2025-10-17 00:06:54 +0530

    Fix: Rename public to static folder for deployment

--- app.py
+++ app.py
@@ -7,13 +7,7 @@ DB_PATH = os.path.join(APP_DIR, 'ctf.db')
 
 
 
-app = Flask(
-    __name__,
-    template_folder='templates',
-    static_folder=os.path.join(APP_DIR, 'public'),  # <-- serve public/
-    static_url_path='/static'                       # <-- maps to /static in URL
-)
-
+app = Flask(__name__)
 
 
 # --- Database Setup ---
